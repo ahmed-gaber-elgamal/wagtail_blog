1a8e7a28666e90783aee55bec9d6911f26528dfb
ahmed-gaber-elgamal <yakan44444@gmail.com>
2020-07-27 21:07:36 +0200

    edit homepage and add bootstrp theme

--- home/models.py
+++ home/models.py
@@ -2,13 +2,34 @@ from django.db import models
 
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
-
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 class HomePage(Page):
+    max_count = 1
+    banner_title = models.CharField(max_length=250, blank=False, null=True)
+    banner_subtitle = RichTextField(features=['bold', 'italic'])
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    banner_cta = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
     body = RichTextField(blank=True)
     content_panels = Page.content_panels + [
-        FieldPanel('body', classname='full')
+        FieldPanel('banner_title'),
+        FieldPanel('banner_subtitle'),
+        ImageChooserPanel('banner_image'),
+        PageChooserPanel('banner_cta'),
+        FieldPanel('body', classname='full'),
     ]
     def get_context(self, request):
         context = super().get_context(request)
