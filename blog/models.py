67100613d35fd9056322491ba7c65916b9667457
ahmed-gaber-elgamal <yakan44444@gmail.com>
2020-07-27 05:58:44 +0200

    add categories

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django import forms
 from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
-from modelcluster.fields import ParentalKey
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
 from modelcluster.contrib.taggit import ClusterTaggableManager
 from taggit.models import TaggedItemBase
 from wagtail.snippets.models import register_snippet
@@ -30,6 +31,7 @@ class BlogPage(Page):
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
     tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+    categories = ParentalManyToManyField("blog.BlogCategory", blank=True)
 
     def main_image(self):
         gallery_item = self.gallery_images.first()
@@ -45,8 +47,8 @@ class BlogPage(Page):
         MultiFieldPanel([
             FieldPanel('date'),
             FieldPanel('tags'),
-            InlinePanel("post_author", label='Author', max_num=1)
-
+            InlinePanel("post_author", label='Author', max_num=1),
+            FieldPanel("categories", widget=forms.CheckboxSelectMultiple)
         ], heading='Blog information'),
         FieldPanel('intro'),
         FieldPanel('body', classname='full'),
@@ -109,3 +111,23 @@ class BlogAuthor(models.Model):
         verbose_name = "Post Author"
         verbose_name = "Post Authors"
 register_snippet(BlogAuthor)
+
+@register_snippet
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=250)
+    icon = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    panels = [
+        FieldPanel('name'),
+        ImageChooserPanel('icon'),
+    ]
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name_plural = "blog categories"
